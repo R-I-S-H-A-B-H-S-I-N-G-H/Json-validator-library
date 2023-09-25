f9c1e8fc6253585060b9e350b4a8315512f82bce
rishabh <rishabhsingh2305@gmail.com>
2023-09-25 20:37:46 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const Ajv = require("ajv");
+const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+
+class JsonValidate {
+	constructor(_schema) {
+		this.schema = _schema;
+	}
+
+	validate(_data) {
+		const validate = ajv.compile(this.schema);
+
+		const valid = validate(_data);
+		if (!valid) {
+			console.log(validate.errors);
+		} else {
+			console.log("SCHEMA IS GOOD");
+		}
+	}
+}
+window.JsonValidate = (schema) => {
+	return new JsonValidate(schema);
+};
